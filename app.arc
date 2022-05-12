5b5851bb051a96d0f4781bdafadbf079e50d7ef8
Aili Huber <alisha.huber@gmail.com>
2022-05-12 17:10:27 -0400

    initial commit

---
+++ app.arc
@@ -0,0 +1,20 @@
+@app
+henslowe-remix-9ce7
+
+@http
+/*
+  method any
+  src server
+
+@static
+
+@tables
+user
+  pk *String
+
+password
+  pk *String # userId
+
+note
+  pk *String  # userId
+  sk **String # noteId
---
+++ app/routes/index.tsx
@@ -0,0 +1,68 @@
+// import { Form } from "@remix-run/react";
+// import { SocialsProvider } from "remix-auth-socials";
+
+// const CONTAINER_STYLES = {
+//   width: "100%",
+//   height: "100vh",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+// };
+
+// const BUTTON_STYLES = {
+//   padding: "15px 25px",
+//   background: "#dd4b39",
+//   border: "0",
+//   outline: "none",
+//   cursor: "pointer",
+//   color: "white",
+//   fontWeight: "bold",
+// };
+
+// const IndexPage = () => {
+//   return (
+//     <Form
+//       method="post"
+//       action={`/auth/${SocialsProvider.GOOGLE}`}
+//       style={CONTAINER_STYLES}
+//     >
+//       <button style={BUTTON_STYLES}>Login with Google</button>
+//     </Form>
+//   );
+// };
+
+// export default IndexPage;
+import { Form } from "@remix-run/react";
+import { SocialsProvider } from "remix-auth-socials";
+
+const CONTAINER_STYLES = {
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const BUTTON_STYLES = {
+  padding: "15px 25px",
+  background: "#dd4b39",
+  border: "0",
+  outline: "none",
+  cursor: "pointer",
+  color: "white",
+  fontWeight: "bold",
+};
+
+const IndexPage = () => {
+  return (
+    <Form
+      method="post"
+      action={`/auth/${SocialsProvider.GOOGLE}`}
+      style={CONTAINER_STYLES}
+    >
+      <button style={BUTTON_STYLES}>Login with Google</button>
+    </Form>
+  );
+};
+
+export default IndexPage;
